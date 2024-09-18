93a13e665fffa234c2644a2ea54a545252702671
krigoll <kristian.gollasch@gmail.com>
2024-09-18 13:50:49 +0200

    add updating and deleting subject

--- SystemZarzadzaniaKorepetycjami_BackEnd/Repositories/Implementations/TeacherSalaryRepository.cs
+++ SystemZarzadzaniaKorepetycjami_BackEnd/Repositories/Implementations/TeacherSalaryRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using SystemZarzadzaniaKorepetycjami_BackEnd.Models;
 using SystemZarzadzaniaKorepetycjami_BackEnd.Repositories.Interfaces;
 using Task = System.Threading.Tasks.Task;
@@ -18,5 +19,49 @@ namespace SystemZarzadzaniaKorepetycjami_BackEnd.Repositories.Implementations
             await _context.TeacherSalary.AddRangeAsync(teacherSalaryList);
             await _context.SaveChangesAsync();
         }
+
+        public async Task<TeacherSalary> FindByTeacherAndSubjectAsync(int teacherId, int subjectLevelId)
+        {
+            return await _context.TeacherSalary
+                .FirstOrDefaultAsync(ts => ts.IdTeacher == teacherId && ts.IdTeacherSalary == subjectLevelId);
+        }
+
+        public async Task UpdateTeacherSalaryAsync(TeacherSalary teacherSalary)
+        {
+            _context.TeacherSalary.Update(teacherSalary);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task CreateUpdateDeleteTeacherSalaryByPersonAsync(List<TeacherSalaryDTO> teacherSalaryDTO, Person person)
+        {
+        foreach (var teacherSalary in teacherSalaryDTO)
+        {
+            var ts = await _context.TeacherSalary.FirstOrDefaultAsync(t =>
+                t.IdTeacher == person.IdPerson && t.IdSubject == teacherSalary.Subject_LevelId);
+            if (ts == null)
+            {
+                if (teacherSalary.HourlyRate > 0)
+                {
+                    var newTeacherSalary = new TeacherSalary(teacherSalary.HourlyRate, person.IdPerson,teacherSalary.Subject_LevelId);
+                    await _context.TeacherSalary.AddAsync(newTeacherSalary); 
+                }   
+            }
+            else
+            {
+                if (teacherSalary.HourlyRate > 0)
+                {
+                    ts.SetHourlyRate(teacherSalary.HourlyRate);
+                    _context.TeacherSalary.Update(ts);
+                }
+                else
+                {
+                    _context.TeacherSalary.Remove(ts);
+                }
+            }
+        }
+
+        await _context.SaveChangesAsync(); 
+        }
+
     }
 }
--- SystemZarzadzaniaKorepetycjami_BackEnd/Repositories/Interfaces/ITeacherSalaryRepository.cs
+++ SystemZarzadzaniaKorepetycjami_BackEnd/Repositories/Interfaces/ITeacherSalaryRepository.cs
@@ -6,5 +6,8 @@ namespace SystemZarzadzaniaKorepetycjami_BackEnd.Repositories.Interfaces
     public interface ITeacherSalaryRepository
     {
         public Task AddTeacherSalaryAsync(List<TeacherSalary> teacherSalaryList);
+        public Task<TeacherSalary> FindByTeacherAndSubjectAsync(int teacherId, int subjectLevelId);
+        public Task UpdateTeacherSalaryAsync(TeacherSalary teacherSalary);
+        public Task CreateUpdateDeleteTeacherSalaryByPersonAsync(List<TeacherSalaryDTO> teacherSalaryDTO, Person person);
     }
 }
