43d86eff1b8a830032f4039f5cfc81be0d0a8e01
DontPanic1303 <118015465+DontPanic1303@users.noreply.github.com>
2024-10-30 11:15:07 +0100

    Get massage bug fix

--- SystemZarzadzaniaKorepetycjami_BackEnd/Repositories/Implementations/MessageRepository.cs
+++ SystemZarzadzaniaKorepetycjami_BackEnd/Repositories/Implementations/MessageRepository.cs
@@ -1,61 +1,63 @@
-﻿using SystemZarzadzaniaKorepetycjami_BackEnd.DTOs;
+﻿using Microsoft.EntityFrameworkCore;
+using SystemZarzadzaniaKorepetycjami_BackEnd.DTOs;
 using SystemZarzadzaniaKorepetycjami_BackEnd.Models;
-using Task = System.Threading.Tasks.Task;
 using SystemZarzadzaniaKorepetycjami_BackEnd.Repositories.Interfaces;
-using Microsoft.EntityFrameworkCore;
+using Task = System.Threading.Tasks.Task;
 
 namespace SystemZarzadzaniaKorepetycjami_BackEnd.Repositories.Implementations
 {
     public class MessageRepository : IMessageRepository
     {
         private readonly SZKContext _context;
-	
-	    public MessageRepository(SZKContext context)
-	    {
-		    _context = context;
-	    }
-	
-	    public async Task<List<ConversationDTO>> GetConversationsByUserAsync(Person user)
-	    {
+
+        public MessageRepository(SZKContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ConversationDTO>> GetConversationsByUserAsync(Person user)
+        {
             var conversations = await _context.Message
-            .Where(m => m.Sender == user.IdPerson || m.Receiver == user.IdPerson)
-            .Select(m => new
-            {
-                CorrespondentId = m.Sender == user.IdPerson ? m.Receiver : m.Sender
-            })
-            .Distinct()
-            .Join(_context.Person,
-                msg => msg.CorrespondentId,
-                person => person.IdPerson,
-                (msg, person) => new ConversationDTO
+                .Where(m => m.Sender == user.IdPerson || m.Receiver == user.IdPerson)
+                .Select(m => new
                 {
-                    UserId = user.IdPerson,
-                    CorespondentId = person.IdPerson,
-                    CorespondentName = person.Name + " " + person.Surname
+                    CorrespondentId = m.Sender == user.IdPerson ? m.Receiver : m.Sender
                 })
-            .ToListAsync();
+                .Distinct()
+                .Join(_context.Person,
+                    msg => msg.CorrespondentId,
+                    person => person.IdPerson,
+                    (msg, person) => new ConversationDTO
+                    {
+                        UserId = user.IdPerson,
+                        CorespondentId = person.IdPerson,
+                        CorespondentName = person.Name + " " + person.Surname
+                    })
+                .ToListAsync();
             return conversations;
-	    }
-	
-	    public async Task<List<MessageDTO>> GetConversationBySenderAndReciverAsync(Person user, Person corespondent)
-	    {
-		    var messages = await _context.Message
-			    .Where(m => (m.Sender == user.IdPerson || m.Receiver == corespondent.IdPerson) || (m.Sender == corespondent.IdPerson || m.Receiver == user.IdPerson))
-			    .OrderBy(m => m.Date)
-			    .Select(m => new MessageDTO {
-				    SenderId = m.Sender,
-				    ReceiverId = m.Receiver,
-				    Date = m.Date.ToString("yyyy-MM-dd HH:mm"),
-				    Content = m.Content
-			    })
-			    .ToListAsync();
-		    return messages;
-	    }
-	
-	    public async Task AddMessageAsync(Message newMessage)
-	    {
-		    await _context.Message.AddAsync(newMessage);
-		    await _context.SaveChangesAsync();
-	    }
+        }
+
+        public async Task<List<MessageDTO>> GetConversationBySenderAndReciverAsync(Person user, Person corespondent)
+        {
+            var messages = await _context.Message
+                .Where(m => (m.Sender == user.IdPerson && m.Receiver == corespondent.IdPerson) ||
+                            (m.Sender == corespondent.IdPerson && m.Receiver == user.IdPerson))
+                .OrderBy(m => m.Date)
+                .Select(m => new MessageDTO
+                {
+                    SenderId = m.Sender,
+                    ReceiverId = m.Receiver,
+                    Date = m.Date.ToString("yyyy-MM-dd HH:mm"),
+                    Content = m.Content
+                })
+                .ToListAsync();
+            return messages;
+        }
+
+        public async Task AddMessageAsync(Message newMessage)
+        {
+            await _context.Message.AddAsync(newMessage);
+            await _context.SaveChangesAsync();
+        }
     }
 }
