352950a60177bc0fe27ba1d5ed958ef4aaa5f610
krigoll <kristian.gollasch@gmail.com>
2024-11-22 14:49:29 +0100

    fix

---
+++ SystemZarzadzaniaKorepetycjami_BackEnd/Models/AssignmentOnTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SystemZarzadzaniaKorepetycjami_BackEnd.Models
+{
+public partial class AssignmentOnTest
+{
+    public int IdTest { get; private set; }
+    public int IdAssignment { get; private set; }
+}
+}
