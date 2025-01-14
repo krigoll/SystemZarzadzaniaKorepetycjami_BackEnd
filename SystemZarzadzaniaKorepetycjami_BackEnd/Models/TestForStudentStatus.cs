983be5b58f97102a4b9634d3ee0f4584bedd3a3e
DontPanic1303 <118015465+DontPanic1303@users.noreply.github.com>
2025-01-14 17:07:41 +0100

    Juz masz

---
+++ SystemZarzadzaniaKorepetycjami_BackEnd/Models/TestForStudentStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace SystemZarzadzaniaKorepetycjami_BackEnd.Models;
+
+public class TestForStudentStatus
+{
+    private TestForStudentStatus()
+    {
+        TestForStudent = new HashSet<TestForStudent>();
+    }
+
+    public int IdTestForStudentStatus { get; }
+    public string Status { get; }
+
+    public virtual ICollection<TestForStudent> TestForStudent { get; private set; }
+}
